d2b0f991f28611b0bfbf4450261681f604e1ab66
Taral K. Jella <19jellat@elmbrookstudents.org>
2018-02-16 12:05:42 -0600

    Test1

---
+++ src/GreeterTest.java
@@ -0,0 +1,14 @@
+
+public class GreeterTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		GreeterClass[] greet = new GreeterClass[7];
+		
+		for (int i = 0; i < 7; i++){
+			System.out.println(greet[i].toString());
+		}
+	}
+
+}
